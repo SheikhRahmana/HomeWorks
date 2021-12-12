fbd587ccba61ecdbe4a403b117d76c224cb45caa
SheikhRahmana <rakhur31@gmail.com>
2021-12-12 10:08:33 -0500

    Homeworksss

---
+++ src/day3/Home.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class Home {
+	static String name;
+	static String address;
+	public Home(String name,String address) {
+	this.name=name;
+	this.address=address;
+	}
+	public void getNme() {
+	System.out.println(name);	
+	}
+	public void getAddrs() {
+		System.out.println(address);
+	}
+
+}
